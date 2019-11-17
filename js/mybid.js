3fc9038c0907ebf845e2ba7b4d2952465ada40f5
hoangman188 <44186580+hoangman188@users.noreply.github.com>
2019-11-17 19:05:28 +0700

    man

--- js/mybid.js
+++ js/mybid.js
@@ -1,3 +1,14 @@
+
+$(document).ready(function(){
+    var sign = document.getElementById('signin');
+    var dialogSign = document.getElementById('dialog_sign');
+    // “Update details” button opens the <dialog> modally
+    sign.addEventListener('click', function() {
+        dialogSign.showModal();
+
+    });
+});
+
 // (function (){
 //     var sign = document.getElementById('singin');
 //     var dialogSign = document.getElementById('dialog_sign');
@@ -7,26 +18,27 @@
 //       dialogSign.showModal();
 //     });
 // })();
-$(document).ready(function() {
-  $('a.login-window').click(function() {
-      //lấy giá trị thuộc tính href - chính là phần tử "#login-box"
-      var loginBox = $(this).attr('href');
+// $(document).ready(function() {
+//   $('a.login-window').click(function() {
+//       //lấy giá trị thuộc tính href - chính là phần tử "#login-box"
+//       var loginBox = $(this).attr('href');
 
-      //cho hiện hộp đăng nhập trong 300ms
-      $(loginBox).fadeIn(300);
+//       //cho hiện hộp đăng nhập trong 300ms
+//       $(loginBox).fadeIn(300);
 
-      // thêm phần tử id="over" vào sau body
-      $('body').append('<div id="over">');
-      $('#over').fadeIn(300);
+//       // thêm phần tử id="over" vào sau body
+//       $('body').append('<div id="over">');
+//       $('#over').fadeIn(300);
 
-      return false;
-});
+//       return false;
+// });
 
-// khi click đóng hộp thoại
-$(document).on('click', "a.close, #over", function() {
-     $('#over, .login').fadeOut(300 , function() {
-         $('#over').remove();
-     });
-    return false;
-});
-});
+// // khi click đóng hộp thoại
+// $(document).on('click', "a.close, #over", function() {
+//      $('#over, .login').fadeOut(300 , function() {
+//          $('#over').remove();
+//      });
+//     return false;
+// });
+// });
+
---
+++ js/product.js
@@ -0,0 +1,32 @@
+var time = 20;    
+
+$(document).ready(function(){
+    var laptop=document.getElementById('categorylaptop');
+    laptop.addEventListener('click', function() {
+    $(".tablet").hide(time);
+    $(".mobile").hide(time);
+    $('.laptop').show(time);
+
+});
+    var all = document.getElementById('categoryall');
+    all.addEventListener('click',function(){
+        $('.tablet').show(time);
+        $('.mobile').show(time);
+        $('.laptop').show(time);
+    });
+
+    var mobile = document.getElementById('categorymobile');
+    mobile.addEventListener('click',function(){
+        $('.tablet').hide(time);
+        $('.mobile').show(time);
+        $('.laptop').hide(time);
+    });
+    var tablet = document.getElementById('categorytablet');
+    tablet.addEventListener('click',function(){
+        $('.tablet').show(time);
+        $('.mobile').hide(time);
+        $('.laptop').hide(time);
+    });
+
+});
+
