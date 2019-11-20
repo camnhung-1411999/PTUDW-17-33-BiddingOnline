b74eeb141e62934fd3a2f7296b5c9ab894c54bd9
camnhung-1411999 <55842532+camnhung-1411999@users.noreply.github.com>
2019-11-20 22:15:46 +0700

    dialog sản phẩm

--- js/modal.js
+++ js/modal.js
@@ -1,69 +1,225 @@
-$(document).ready(function () {
+// //favorite product
+var heart = document.querySelector('.heart');
 
-    
-    $('#burger').click(function () {
-        $('#nava').slideToggle();
-        $('#searchform').slideUp();
-        $('#mega-menu').slideUp();
-    });
-    
-    $('#search-click').click(function () {
+heart.addEventListener('click', function() {
+  this.classList.toggle('loved');
+}, false);
+// end
 
-        $('#mega-menu').slideUp();
-        $('#nava').slideUp();
-        $('#searchform').slideToggle();
-    });
 
-    $('#search-click-list').click(function () {
-        $('#mega-menu').slideUp();
-        $('#searchform').slideToggle();
-    });
 
-    $('#mega-menu-btn').click(function () {
-        $('#searchform').slideUp();
-        $('#mega-menu').slideToggle();
-    });
+(function ($) {
+  $.fn.niceNumber = function(options) {
+    var settings = $.extend({
+      autoSize: false,
+      autoSizeBuffer: 1,
+      buttonDecrement: '-',
+      buttonIncrement: "+",
+      buttonPosition: 'around'
+    }, options);
 
-    $('#myTab a').click(function (e) {
-        e.preventDefault();
-        $(this).tab('show');
-    });
+    return this.each(function(){
+      var currentInput = this,
+          $currentInput = $(currentInput),
+          attrMax = 999,
+          attrMin = 0;
 
-    $("#main-image").elevateZoom({
-        scrollZoom: true
-    });
+      // Handle max and min values
+      if (
+        typeof $currentInput.attr('max') !== typeof undefined
+        && $currentInput.attr('max') !== false
+      ) {
+        attrMax = parseFloat($currentInput.attr('max'));
+      }
 
-    $('.side-picture').click(function () {
-        
-        var showing = $(this).find("img").attr("src");
-        $('.side-picture').removeClass('active');
-        $(this).addClass('active');
-        $('#main-image').fadeOut(function () {
-            $(this).attr('src', showing);
-            $(this).fadeIn();
-            $(this).elevateZoom({
-                scrollZoom: true
-            });
+      if (
+        typeof $currentInput.attr('min') !== typeof undefined
+        && $currentInput.attr('min') !== false
+      ) {
+        attrMin = parseFloat($currentInput.attr('min'));
+      }
+
+      // Fix issue with initial value being < min
+      if (
+        attrMin
+        && !currentInput.value
+      ) {
+        $currentInput.val(attrMin);
+      }
+
+      // Generate container
+      var $inputContainer = $('<div/>',{
+          class: 'nice-number'
+        })
+        .insertAfter(currentInput);
+
+      // Generate interval (object so it is passed by reference)
+      var interval = {};
+
+      // Generate buttons
+      var $minusButton = $('<button/>')
+        .attr('type', 'button')
+        .html(settings.buttonDecrement)
+        .on('mousedown mouseup mouseleave', function(event){
+          changeInterval(event.type, interval, function(){
+            if (
+              attrMin == null
+              || attrMin < parseFloat(currentInput.value)
+            ) {
+              currentInput.value--;
+            }
+          });
+
+          // Trigger the input event here to avoid event spam
+          if (
+            event.type == 'mouseup'
+            || event.type == 'mouseleave'
+          ) {
+            $currentInput.trigger('input');
+          }
         });
-        //    $('#main-image').attr('src', showing);
 
+      var $plusButton = $('<button/>')
+        .attr('type', 'button')
+        .html(settings.buttonIncrement)
+        .on('mousedown mouseup mouseleave', function(event){
+          changeInterval(event.type, interval, function(){
+            if (
+              attrMax == null
+              || attrMax > parseFloat(currentInput.value)
+            ) {
+              currentInput.value++;
+            }
+          });
+
+          // Trigger the input event here to avoid event spam
+          if (
+            event.type == 'mouseup'
+            || event.type == 'mouseleave'
+          ) {
+            $currentInput.trigger('input');
+          }
+        });
+
+      // Append elements
+      switch (settings.buttonPosition) {
+        case 'left':
+          $minusButton.appendTo($inputContainer);
+          $plusButton.appendTo($inputContainer);
+          $currentInput.appendTo($inputContainer);
+          break;
+        case 'right':
+          $currentInput.appendTo($inputContainer);
+          $minusButton.appendTo($inputContainer);
+          $plusButton.appendTo($inputContainer);
+          break;
+        case 'around':
+        default:
+          $minusButton.appendTo($inputContainer);
+          $currentInput.appendTo($inputContainer);
+          $plusButton.appendTo($inputContainer);
+          break;
+      }
+
+      // Nicely size input
+      if (settings.autoSize) {
+        $currentInput.width(
+          $currentInput.val().length+settings.autoSizeBuffer+"ch"
+        );
+        $currentInput.on('keyup input',function(){
+          $currentInput.animate({
+            'width': $currentInput.val().length+settings.autoSizeBuffer+"ch"
+          }, 200);
+        });
+      }
     });
+  };
 
+  function changeInterval(eventType, interval, callback) {
+    if (eventType == "mousedown") {
+      interval.timeout = setTimeout(function(){
+        interval.actualInterval = setInterval(function(){
+          callback();
+        }, 100);
+      }, 200);
+      callback();
+    } else {
+      if (interval.timeout) {
+        clearTimeout(interval.timeout);
+      }
+      if (interval.actualInterval) {
+        clearInterval(interval.actualInterval);
+      }
+    }
+  }
+}(jQuery));
 
-    //    var mySwiper = new Swiper('.swiper-container', {
-    //        // Optional parameters
-    //        direction: 'vertical',
-    //        loop: true,
-    //
-    //        // If we need pagination
-    //        pagination: '.swiper-pagination',
-    //
-    //        // Navigation arrows
-    //        nextButton: '.swiper-button-next',
-    //        prevButton: '.swiper-button-prev',
-    //
-    //        // And if we need scrollbar
-    //        scrollbar: '.swiper-scrollbar'
-    //    });
+$('input#add-to-cart-qty').niceNumber();
 
+$('input#add-to-cart-qty').keydown(function (e) {
+    if (e.keyCode == 190 || e.keyCode == 110 || e.keyCode == 69 || e.keyCode == 189 || e.keyCode == 109) {
+        e.preventDefault();
+    }
+
+    if (isNaN(parseInt(window.getSelection()))) {
+        var code = (e.keyCode >= 96) ? e.keyCode - 48 : e.keyCode;
+        var keyval = parseInt(String.fromCharCode(code));
+
+        if (!isNaN(keyval)) {
+            if ($(this).val() + keyval > parseInt($(this).attr('max'))) {
+                e.preventDefault();
+            }
+        }
+    }
 });
+$('input#add-to-cart-qty').keyup(function (e) {
+    if ($(this).val() != '') {
+        $(this).val(parseInt($(this).val()));
+    }
+});
+// ------------- //
+
+
+$('#text').hide();
+
+$('#subscribe-btn').change(function() {
+    // use the :checked selector to find any that are checked
+    if($("#subscribe-btn").is(':checked')) {
+        $('#text').slideDown();
+    } else {
+        $('#text').slideUp();
+    }
+ });
+
+/* Images */
+$('.thumbnail').on('click', function() {
+  var clicked = $(this);
+  var newSelection = clicked.data('big');
+  var $img = $('.primary').css("background-image","url(" + newSelection + ")");
+  clicked.parent().find('.thumbnail').removeClass('selected');
+  clicked.addClass('selected');
+  $('.primary').empty().append($img.hide().fadeIn(500));
+  return false;
+});
+
+
+
+
+$('button.add-to-cart').on('click', function(){
+  var cartBtn = $(this);
+  cartBtn.addClass('success').text( "Success!" ).attr('disabled',true);
+  setTimeout(function() { cartBtn.removeClass('success').addClass('active').text( "Add to Cart" ).attr('disabled', false);}, 1500);
+});
+
+
+$('#favorite-button').on('click', function(){
+    $(this).toggleClass('active');
+})
+
+function changeImg(){
+    var showing = $('.side-picture').find("img").attr("src");
+    document.getElementById('main-image').src=""+showing;
+   var  mainImg=document.getElementById('main-image');
+    //alert(mainImg.src);
+    
+}
